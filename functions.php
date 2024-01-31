2c9a02bbdd0d665c5e2fa0b09e45f1a250be8f80
collinberg <berg.collin@gmail.com>
2024-01-31 11:21:08 -0500

    Modal Size bigger

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@
 
 if ( ! defined( '_S_VERSION' ) ) {
 	// Replace the version number of the theme on each release.
-	define( '_S_VERSION', '1.1.4' );
+	define( '_S_VERSION', '1.2.2' );
 }
 
 if ( ! function_exists( 'sji_theme_setup' ) ) :
