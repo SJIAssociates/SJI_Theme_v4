dd0f40369ced6b9d9ef964f726bef07317737ca5
collinberg <berg.collin@gmail.com>
2024-04-23 10:09:48 -0400

    Video Grid can now have 2 or 3 rows

--- flex/modules/video-grid.php
+++ flex/modules/video-grid.php
@@ -1,13 +1,14 @@
 <?php 
 
 $video_columns = get_field('video_columns');
+$cols = get_sub_field('columns');
 
 ?>
 
 <div class="video-grid" style="background-color:<?php echo get_sub_field('background_color'); ?>">
     <div class="container">
         <?php if ( have_rows('video_columns') ) :  ?>
-        <div class="video-grid__grid">
+        <div class="video-grid__grid <?php echo $cols; ?>">
             <?php while ( have_rows('video_columns') ): the_row() ?>
             <figure class="video-grid__item">
                 <video class="video-grid__vid" playsinline autoplay loop muted>
