28aed681f0de360b13b290fb096f717741009a2a
collinberg <berg.collin@gmail.com>
2024-05-08 10:09:13 -0400

    fixed the looping setting not working

--- flex/modules/captioned-banner-with-videos.php
+++ flex/modules/captioned-banner-with-videos.php
@@ -5,10 +5,12 @@ $color = get_sub_field('background_color');
 $align = get_sub_field('align_copy');
 $padding = get_sub_field('padding');
 $vidoWidth = get_sub_field('column_split') ?: 'col-lg';
+$swap_orientation = (get_sub_field('swap_orientation') == true )? 'swap' : "normal";
+$loop = get_sub_field('video_loop')
 
 ?>
 
-<section class="captioned-banner <?php echo $padding; ?>"<?php if( !empty($color) ){ 
+<section class="captioned-banner <?php echo $padding . ' ' . $swap_orientation; ?>"<?php if( !empty($color) ){ 
     echo "style='background-color: $color;'";
     } ?> 
     data-module="Captioned Video">
@@ -20,14 +22,14 @@ $vidoWidth = get_sub_field('column_split') ?: 'col-lg';
                     while (have_rows('videos')) : the_row();
                         $video = get_sub_field('video');
                     ?>
-                        <video playsinline autoplay muted <?php if (get_sub_field('video_loop') == 'loop') : echo 'loop'; endif; ?>>
+                        <video playsinline autoplay muted <?php if ($loop == 'loop') : echo 'loop'; endif; ?>>
                             <source src="<?= $video['url'] ?>" type="video/mp4">
                         </video>
                     <?php endwhile; ?>
                 </div>
             <?php endif; ?>
             <?php if(get_sub_field('caption')): ?>
-                <div class='col-lg <?php echo $align; ?>'>
+                <div class='col-lg <?php echo $align;?> <?php echo  $swap_orientation; ?>'>
                     <?php the_sub_field('caption'); ?>
                 </div>
             <?php endif; ?>
